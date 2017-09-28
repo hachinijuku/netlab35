0a75f95e4aa6bb5253f29f4f0b9aeb9ed34522f1
Joseph N. Wilson <jnw@cise.ufl.edu>
2017-09-28 08:44:53 -0400

    typos, typos

--- src/cancel_reservations.py
+++ src/cancel_reservations.py
@@ -27,11 +27,11 @@ def main():
     api = Client()
 
     for x in args.res_ids:
-        print(str(res_id))
+        print(str(x))
     yes_no = input('Do you want to cancel these reservations (y/n)? ')
     if yes_no[0].lower() == 'y':
 
-        for res-id in args.res_ids:
+        for res_id in args.res_ids:
             result = api.cancel(res_id)
             print('Reservation of pod ' + str(pod_names[index]) + ':'+str(datetime.now())+':'+result)
             
